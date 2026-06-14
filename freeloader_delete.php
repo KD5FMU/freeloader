935c21f74a5ac0bdecb33f926e4bd7544d623508
n5ad <n5ad@att.net>
2026-06-14 10:20:17 -0500

    Update freeloader_delete.php

--- freeloader_delete.php
+++ freeloader_delete.php
@@ -1,7 +1,9 @@
+<?php
 // freeloader_delete.php
 // file deletion utility for freeloader file upload utility
 // created by James N5AD
 // June 2026
+?>
 
 <?php if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['file'])) { $filename = basename($_POST['file']); $path = "/my_uploads/" . $filename;
     
